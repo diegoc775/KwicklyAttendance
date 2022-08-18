cb9c87281c6084b851df6a35bb80d0a854f35a8b
Diego Cobos <diego.cobos.18@cnu.edu>
2022-08-18 13:06:28 -0400

    Initial commit

---
+++ app/src/main/java/com/example/kwicklyattendance/AttendanceDetailsActivity.kt
@@ -0,0 +1,11 @@
+package com.example.kwicklyattendance
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AttendanceDetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_attendance_details)
+    }
+}
